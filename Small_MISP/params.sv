211db0efc99f72b41f5d07c17e0dbd0c925d76c0
Mohannad <mohannadmostafa@outlook.com>
2016-10-08 17:23:29 -0400

    removing a folder

--- Small_MISP/params.sv
+++
@@ -1,6 +0,0 @@
-    parameter mem_file = "SimpleAdd.x";
-	parameter [31:0] mem_start = 32'h80020000;
-	parameter mem_depth = 2**20; // 1 MB
-	parameter mem_delay=0;
-	typedef enum { sz_byte, sz_word, sz_4word, sz_8word } mem_access_sizes;
-
